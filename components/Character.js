7079a96e7a4738f7e2bd221c7803f209dce03196
Brian <dev.bnguyen@gmail.com>
2022-01-04 00:29:08 +1100

    Add character component

---
+++ components/Character.js
@@ -0,0 +1,33 @@
+const template = document.createElement('template');
+
+template.innerHTML = `
+    <style>
+        .character {
+            border: 5px solid yellow;
+        }
+    </style>
+
+    <div class="character">
+        <img/>
+    </div>
+`
+
+// Create a class for the element
+class Character extends HTMLElement {
+    constructor() {
+        // Always call super first in constructor
+        super();
+
+        // Create a shadow root
+        const shadow = this.attachShadow({mode: 'open'});
+
+        // Attach the created template element to the shadow dom
+        shadow.appendChild(template.content.cloneNode(true));
+
+        // Props
+        shadow.querySelector('img').src = this.getAttribute('sprite');   
+    }
+}
+
+// Define the new element
+window.customElements.define('ms-character', Character)
