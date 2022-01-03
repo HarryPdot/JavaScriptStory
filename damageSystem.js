7c31a3456a87b210dde3fca727bc26edf54940e7
HarryP <harrypham__@hotmail.com>
2022-01-03 23:25:09 +1100

    Add monsters/wavelevels

--- damageSystem.js
+++ damageSystem.js
@@ -1,9 +1,11 @@
-console.log("yes")
-
 var hpBar = document.querySelector(".health-bar")
 // var characterOne = document.querySelector(".character-one")
 // var characterTwo = document.querySelector(".character-two")
-var boss = document.querySelector(".boss")
+var bossImage = document.querySelector(".boss")
+var mobImage = document.querySelector(".mob")
+var monsterNameText = document.querySelector(".monsterNameText")
+
+
 
 // character stats
 let partyOne = {
@@ -17,33 +19,78 @@ let partyTwo = {
     }
 }
 
-// boss hp
-let bossHp = 100
-let bossHpText = bossHp
-let bossLevel = 1
-hpBar.textContent = Number(bossHp * bossLevel)
-// character damage range
-var range = 5
+// current monster
+let monstersOrder = ["snail", "slime", "mushroom", "boss"]
+let monsters = [
+        snail= {
+            name: "snail",
+            hp: 30
+        },
+        slime= {
+            name: "slime",
+            hp: 80
+        },
+        mushroom= {
+            name: "mushroom",
+            hp: 150
+        },
+        boss= {
+            name: "boss",
+            hp: 400
+        }
+]
+let counter = 1
+let mobHp = ""
+let waveLevel = 1
 
+// handles current monster
+function currentMonster() {
+    if (counter % 4 == 1) {
+        monsterNameText.textContent = "snail"
+        mobHp = Number(monsters[0].hp * waveLevel)
+    } else if ( counter % 4 == 2) {
+        monsterNameText.textContent = "slime"
+        mobHp = Number(monsters[1].hp * waveLevel)
+    } else if ( counter % 4 == 3) {
+        monsterNameText.textContent = "mushroom"
+        mobHp = Number(monsters[2].hp * waveLevel)
+    } else if ( counter % 4 == 0) {
+        monsterNameText.textContent = "boss"
+        mobHp = Number(monsters[3].hp * waveLevel)
+    }
+}
+
+currentMonster()
+
+// current monster hp bar
+let mobHpText = mobHp
+// let bossHpText = bossHp
+
+hpBar.textContent = Number(mobHp * waveLevel)
+
+//handles the damage to the monster
 function handleDamage() {
-    console.log(bossHpText)
-    bossHpText = bossHpText - (partyOne.characterOne.range + partyTwo.characterOne.range)
-    console.log("bossHpText", bossHpText)
-    hpBar.textContent = bossHpText
+    mobHp = mobHp - (partyOne.characterOne.range + partyTwo.characterOne.range)
+    console.log("mobHp", mobHp)
+    hpBar.textContent = mobHp
 
-    if (bossHpText === 0) {
-        handleNewBoss()
+    if (mobHp === 0) {
+        handleNewMonster()
     }
 }
 
 // new boss when current defeated
-function handleNewBoss() {
-    bossLevel = bossLevel + 1
-    console.log("bossLevel", bossLevel)
-    bossHpText = bossHp * bossLevel
-    hpBar.textContent = bossHpText
+function handleNewMonster() {
+    if(counter % 4 == 0){
+        waveLevel = waveLevel + 1
+        console.log("waveLevel", waveLevel)
+    }
+    counter ++
+    currentMonster()
+    console.log("counter", counter)
 }
 
 
-setInterval(handleDamage, 1000)
-boss.addEventListener("click", handleDamage);
+setInterval(handleDamage, 500)
+bossImage.addEventListener("click", handleDamage);
+mobImage.addEventListener("click", handleDamage);
