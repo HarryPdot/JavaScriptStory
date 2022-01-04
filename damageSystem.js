4114b8760869b45f4b7b81245c54a1ca2082d4eb
HarryP <harrypham__@hotmail.com>
2022-01-05 00:57:25 +1100

    added experience point system

--- damageSystem.js
+++ damageSystem.js
@@ -3,48 +3,64 @@ var hpBar = document.querySelector(".health-bar")
 // var characterTwo = document.querySelector(".character-two")
 var bossImage = document.querySelector(".boss")
 var mobImage = document.getElementById("mob")
+var centerGrid = document.querySelector(".center-grid")
 var mobNameText = document.querySelector(".mobNameText")
+var currentExperience = document.querySelector(".experience")
+var totalExperience = document.querySelector(".total-experience")
 
 // character stats
 let partyOne = {
     characterOne: {
         level: 1,
         range: 5,
-        experience: 0
+        experience: 0,
+        totalLevelExperience: 50,
+        expRemainder: 0
     }
 }
+
 let partyTwo = {
     characterOne: {
         level: 1,
         range: 5,
-        experience: 0
+        experience: 0,
+        totalLevelExperience: 50,
+        expRemainder: 0
     }
 }
+
 
 // current mob
 let mobOrder = ["mob1", "mob2", "mob3", "boss"]
 let mobs = [
         mob1= {
-            name: "mob1",
-            hp: 30
+            name: "snail",
+            hp: 30,
+            experience: 5
         },
         mob2= {
-            name: "mob2",
-            hp: 80
+            name: "blue snail",
+            hp: 80,
+            experience: 10
+
         },
         mob3= {
-            name: "mob3",
-            hp: 150
+            name: "red snail",
+            hp: 150,
+            experience: 20
         },
         boss= {
-            name: "boss",
-            hp: 400
+            name: "mano",
+            hp: 400,
+            experience: 50
         }
 ]
 
 let counter = 1
 let mobHp = ""
 let waveLevel = 1
+currentExperience.textContent = partyOne.characterOne.experience
+totalExperience.textContent = partyOne.characterOne.totalLevelExperience
 
 // handleMobImages
 var mob1 = '<ms-mob sprite="./assets/images/snail_stand.png" id="mob"/>'
@@ -57,16 +73,16 @@ var render = function (template, node) {
 };
 function currentMob() {
     if (counter % 4 == 1) {
-        mobNameText.textContent = "mob1"
+        mobNameText.textContent = mobs[0].name
         mobHp = Number(mobs[0].hp * waveLevel)
     } else if ( counter % 4 == 2) {
-        mobNameText.textContent = "mob2"
+        mobNameText.textContent = mobs[1].name
         mobHp = Number(mobs[1].hp * waveLevel)
     } else if ( counter % 4 == 3) {
-        mobNameText.textContent = "mob3"
+        mobNameText.textContent = mobs[2].name
         mobHp = Number(mobs[2].hp * waveLevel)
     } else if ( counter % 4 == 0) {
-        mobNameText.textContent = "boss"
+        mobNameText.textContent = mobs[3].name
         mobHp = Number(mobs[3].hp * waveLevel)
     }
 }
@@ -80,10 +96,9 @@ hpBar.textContent = Number(mobHp * waveLevel)
 //handles the damage to the mob
 function handleDamage() {
     mobHp = mobHp - (partyOne.characterOne.range + partyTwo.characterOne.range)
-    console.log("mobHp", mobHp)
     hpBar.textContent = mobHp
 
-    if (mobHp === 0) {
+    if (mobHp <= 0) {
         handleNewMob()
         handleExperience()
     }
@@ -98,7 +113,7 @@ function handleNewMob() {
     counter ++
     currentMob()
     handleMobImages()
-
+    hpBar.textContent = mobHp
     console.log("counter", counter)
 }
 
@@ -118,22 +133,49 @@ function handleMobImages() {
 // handle character level experience
 function handleExperience(){
 
-    console.log("character one exp", partyOne.characterOne.experience)
-    if (counter % 4 == 1) {
-      partyOne.characterOne.experience += 5
-		} else if ( counter % 4 == 2) {
-			partyOne.characterOne.experience += 5
+    if (counter % 4 == 2) {
+      partyOne.characterOne.experience += mobs[0].experience
 		} else if ( counter % 4 == 3) {
-			partyOne.characterOne.experience += 5
+			partyOne.characterOne.experience += mobs[1].experience
 		} else if ( counter % 4 == 0) {
-			partyOne.characterOne.experience += 5
+			partyOne.characterOne.experience += mobs[2].experience
+		} else if ( counter % 4 == 1) {
+			partyOne.characterOne.experience += mobs[3].experience
     }
-    console.log("characterOne",partyOne.characterOne.experience)
-    console.log("characterTwo",partyTwo.characterOne.experience)
+    console.log("character exp", partyOne.characterOne.experience)
+    if(partyOne.characterOne.experience >= partyOne.characterOne.totalLevelExperience) {
+        console.log("level up")
+        handleLevelUp()
+        partyOne.characterOne.experience = partyOne.characterOne.experience % partyOne.characterOne.totalLevelExperience
+        console.log("character exp", partyOne.characterOne.experience)
+    }
+    currentExperience.textContent = partyOne.characterOne.experience
+    totalExperience.textContent = partyOne.characterOne.totalLevelExperience
+}
+
+function handleLevelUp() {
+    let i = partyOne.characterOne.level
+    if(i <= 9) {
+        partyOne.characterOne.totalLevelExperience = Math.floor(partyOne.characterOne.totalLevelExperience * 1.8)
+        partyOne.characterOne.range += 5
+    } else if(i <= 29) {
+        partyOne.characterOne.totalLevelExperience = Math.floor(partyOne.characterOne.totalLevelExperience * 1.7)
+    } else if(i <= 39) {
+        partyOne.characterOne.totalLevelExperience = Math.floor(partyOne.characterOne.totalLevelExperience * 1.5)
+    } else if(i <= 59) {
+        partyOne.characterOne.totalLevelExperience = Math.floor(partyOne.characterOne.totalLevelExperience * 1.3)
+    } else if(i <= 79) {
+        partyOne.characterOne.totalLevelExperience = Math.floor(partyOne.characterOne.totalLevelExperience * 1.2)
+    } else if(i <= 89) {
+        partyOne.characterOne.totalLevelExperience = Math.floor(partyOne.characterOne.totalLevelExperience * 1.2)
+    } else if(i <= 99) {
+        partyOne.characterOne.totalLevelExperience = Math.floor(partyOne.characterOne.totalLevelExperience * 1.1)
+    }
+    console.log("playerOneTotalExp", partyOne.characterOne.totalLevelExperience)
+    partyOne.characterOne.level += 1
+    console.log("level", partyOne.characterOne.level)
 
 }
 
-
-
 setInterval(handleDamage, 500)
-// mobImage.addEventListener("click", handleDamage);
+centerGrid.addEventListener("click", handleDamage);
